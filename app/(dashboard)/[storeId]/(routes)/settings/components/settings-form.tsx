4bf21f153ec8c23022f51c19fd0184fc7f7fa53f
chndrwali <chndrwali@gmail.com>
2024-02-21 19:40:35 +0700

    add: toast

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -1,11 +1,14 @@
 'use client';
 
 import * as z from 'zod';
+import axios from 'axios';
 import { Store } from '@prisma/client';
 import { Trash2 } from 'lucide-react';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useState } from 'react';
+import toast from 'react-hot-toast';
+import { useParams, useRouter } from 'next/navigation';
 
 import { Heading } from '@/components/ui/heading';
 import { Button } from '@/components/ui/button';
@@ -24,8 +27,11 @@ const formSchema = z.object({
 type SettingsFormValues = z.infer<typeof formSchema>;
 
 export const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
-  const [open, setOpen] = useState();
-  const [loading, setLoading] = useState();
+  const params = useParams();
+  const router = useRouter();
+
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const form = useForm<SettingsFormValues>({
     resolver: zodResolver(formSchema),
@@ -33,7 +39,16 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
   });
 
   const onSubmit = async (data: SettingsFormValues) => {
-    console.log(data);
+    try {
+      setLoading(true);
+      await axios.patch(`/api/stores/${params.storeId}`, data);
+      router.refresh();
+      toast.success('Toko di perbarui.');
+    } catch (error) {
+      toast.error('Ada yang salah.');
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
